fd545bfb63428203c4cf2ff7938a9e5dbc058c1c
Matthias Lochmann <59730731+Ma-Loma@users.noreply.github.com>
2026-04-29 09:56:00 +0200

    Create install_packages_locallly.R

---
+++ install_packages_locallly.R
@@ -0,0 +1,19 @@
+.libPaths("M:/R/library-4.6")
+packagelist <- c(
+  "arrow", 
+  "dbplyr",
+  "zoo",
+  "data.table", # till here: not necessary
+  "tidyverse", # from here: necessary for END2DALY
+  "readxl",
+  "purrr",
+  "janitor",
+  "rmarkdown",
+  "flextable",
+  "knitr",
+  "remotes",
+  "terra",
+  "sf",
+  "exactextractr"
+)
+install.packages(packagelist, lib="M:/R/library-4.6")
